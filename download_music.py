86d6347010ff274dda3cd8b70b2fe3306b837b24
Hodaka Suzuki <altitude3190@gmail.com>
2019-08-05 01:34:50 +0900

    update music downloading process

--- download_music.py
+++ download_music.py
@@ -14,17 +14,25 @@ def calc_second(text):
     texts = text.split(':')
     return int(texts[0]) * 60 + int(texts[1])
 
-for index, row in df_s.iterrows():
-    if duration == calc_second(row[2]):
-        req = urllib.request.Request(row[6])
+raw_duration = duration
 
-        xml_resposnse = urllib.request.urlopen(req).read()
+while duration >= raw_duration - 5:
+    for index, row in df_s.iterrows():
+        if duration == calc_second(row[2]):
+            req = urllib.request.Request(row[6])
 
-        os.makedirs("{}/music/{}".format(cur, duration), exist_ok=True)
-        path = "{}/music/{}/{}.mp3".format(cur, duration, row[0].replace(' ', '_'))
+            xml_resposnse = urllib.request.urlopen(req).read()
 
-        with open(path, mode='wb') as f:
-            f.write(xml_resposnse)
+            os.makedirs("{}/music/{}".format(cur, duration), exist_ok=True)
+            path = "{}/music/{}/{}.mp3".format(cur, duration, row[0].replace(' ', '_'))
 
-        print(path)
-        break
+            with open(path, mode='wb') as f:
+                f.write(xml_resposnse)
+
+            print(path)
+            break
+    else:
+        duration =- 1
+        continue
+
+    break
