9d28cffad2b1f73e23817e9d1314fab098141ef8
Hodaka Suzuki <altitude3190@gmail.com>
2019-08-06 01:31:29 +0900

    value round

--- format_json.py
+++ format_json.py
@@ -2,6 +2,8 @@ import pandas as pd
 import json
 import collections as cl
 import numpy
+import math
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 
 # alpha3Codeをindexに設定して読み込む
 csv = pd.read_csv('./data.csv', header=0, index_col=1)
@@ -29,6 +31,45 @@ for i in range(columns.size):
             }
 '''
 
+# 値をまるめる
+
+max = csv[years[-1]].max()
+max_keta = len(str(math.floor(max)))
+
+unit = 0
+keta = 0
+while max_keta - unit > 7 and unit <= 9:
+    if max_keta - unit <= 7:
+        keta = max_keta - unit
+        break
+    unit += 3
+
+# 2,487,045,000,000 13桁
+# 有効数字5桁 - 7桁になるように調整する
+
+# 12 - 9 = 3
+# 12 - 6 = 6
+#
+# 13 - 9 = 4
+# 13 - 6 = 7
+#
+# 14 - 9 = 5
+# 14 - 6 = 8
+#
+# 15 - 9 = 6
+# 15 - 6 = 9
+
+def val_round(val, units):
+    if val == float(math.ceil(val)):
+        val = math.ceil(val)
+    if units == 0:
+        return val
+    val = val / (10 ** units)
+    if val == float(math.ceil(val)):
+        val = math.ceil(val)
+    return(float(Decimal(str(val)).quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)))
+
+
 # data.dataの取得
 data = []
 
@@ -44,7 +85,7 @@ for index, row in csv.iterrows():
     for i in years_index:
         if numpy.isnan(row[i]):
             row[i] = 0
-        hash['values'].append(row[i])
+        hash['values'].append(val_round(row[i], unit)) # 値を挿入
 
     data.append(hash)
 
@@ -56,6 +97,17 @@ json_hash['settings'] = cl.OrderedDict()
 with open('./meta_data.json') as f:
     df = json.load(f)
     json_hash['settings']['layout.title'] = df['title']
+
+layout_text = ''
+if unit == 3:
+    layout_text = 'unit: thousand'
+elif unit == 6:
+    layout_text = 'unit: million'
+elif unit == 9:
+    layout_text = 'unit: billion'
+
+json_hash['settings']['layout.text'] = layout_text
+
 
 # columns
 
